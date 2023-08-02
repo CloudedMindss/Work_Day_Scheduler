dd964f0b89bc1706af2062b881e3535ad3174468
CloudedMindss <87626582+CloudedMindss@users.noreply.github.com>
2023-08-02 17:58:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,23 @@
         }
       });
     }
+
+    $('.time-block').each(function() {
+      const key = $(this).attr('id');
+      const value = localStorage.getItem(key);
+      $(this).children('.description').val(value);
+    });
+    function updateTime() {
+      const dateElement = $('#date');
+      const timeElement = $('#time');
+      const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+      const currentTime = dayjs().format('hh:mm:ss A');
+      dateElement.text(currentDate);
+      timeElement.text(currentTime);
+    }
+    hourlyColor();
+    textEntry();                
+    refreshColor();
+   
+    setInterval(updateTime, 1000);
+  });
