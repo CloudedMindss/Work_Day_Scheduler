be7de819462f680514672c84ac1ca16f4c25bfc5
CloudedMindss <87626582+CloudedMindss@users.noreply.github.com>
2023-08-02 17:56:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
   dayjs.locale(localeSettings);
   $(function () {
     const currentHour = dayjs().format('H');
+
+    function hourlyColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        $(this).toggleClass('past', blockHour < currentHour);
+        $(this).toggleClass('present', blockHour === currentHour);
+        $(this).toggleClass('future', blockHour > currentHour);
+      });
+    }
